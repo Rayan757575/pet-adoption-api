370ab91fb157f8b0194fffe0ce58e9abb16a30c7
Rayan757575 <rayancatapretastl@gmail.com>
2025-11-02 22:21:25 -0300

    refactor: Improve performance of Class logic

--- src/main/java/com/rayancatapreta/pet_adoption_api/model/User.java
+++ src/main/java/com/rayancatapreta/pet_adoption_api/model/User.java
@@ -2,10 +2,12 @@ package com.rayancatapreta.pet_adoption_api.model;
 
 import com.rayancatapreta.pet_adoption_api.enums.Role;
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.security.core.userdetails.UserDetails;
 
+@Data
 @Entity
-@Table(name = "users")
+@Table(name = "users") //avoids problems with reserved words when implementing the database
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,13 +19,15 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    private String name;
+
+    private String lastName;
+
+    private String phone;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
-    private String name;
-
-    private String phone;
 
     // --- Interface fields UserDetails ---
 
