a91007194eba643a92eaefa949ba9db3999e6e33
Rayan757575 <rayancatapretastl@gmail.com>
2026-02-09 02:20:30 -0300

    fix: Updating the register route

--- src/main/java/com/rayancatapreta/pet_adoption_api/security/SecurityConfig.java
+++ src/main/java/com/rayancatapreta/pet_adoption_api/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
                         // Requires the user to be authenticated to access any route other than the login or
                         // registration route
                         .anyRequest().authenticated()
