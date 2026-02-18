1cda0313f168b10c92d6728059747eff16af4860
Rayan757575 <rayancatapretastl@gmail.com>
2026-02-17 21:15:34 -0300

    refactor: improving the writing of the file

--- src/main/java/com/rayancatapreta/pet_adoption_api/security/TokenService.java
+++ src/main/java/com/rayancatapreta/pet_adoption_api/security/TokenService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -18,17 +19,18 @@ import java.time.ZoneOffset;
 @RequiredArgsConstructor
 public class TokenService {
 
-    private static final String ISSUER = "pet-adoption-api";
+    private static final String ISSUER = "pet-adoption-api"; // name of who is creating the token
     @Value("${api.security.token.secret}")
     private String secret;
 
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
+            // return token
             return JWT.create()
                     .withIssuer(ISSUER)
-                    .withSubject(user.getEmail())
-                    .withExpiresAt(genExpirationDate())
+                    .withSubject(user.getEmail()) // the user who is receiving the token
+                    .withExpiresAt(genExpirationDate()) // token's time expiration
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             log.error("Error generating token for user {}: ", user.getEmail(), exception);
@@ -39,6 +41,7 @@ public class TokenService {
     public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
+            // verify the token
             return JWT.require(algorithm)
                     .withIssuer(ISSUER)
                     .build()
@@ -51,6 +54,6 @@ public class TokenService {
     }
 
     private Instant genExpirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00")); // set 2 hours to time expiration
     }
 }
