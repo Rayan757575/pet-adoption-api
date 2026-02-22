d68202c1e4488c2ce62fcbed77ecbfb3bba94219
Rayan757575 <rayancatapretastl@gmail.com>
2026-02-22 02:18:07 -0300

    feat(exception): implement validation error handling and refine status codes

--- src/main/java/com/rayancatapreta/pet_adoption_api/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/rayancatapreta/pet_adoption_api/infra/exception/RestExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.rayancatapreta.pet_adoption_api.infra.exception;
 
+import lombok.NonNull;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice // It tells Spring to watch for exceptions in all controllers.
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -22,5 +30,26 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<ErrorMessageDTO> badCredentialsHandler(BadCredentialsException exception) {
         ErrorMessageDTO errorResponse = new ErrorMessageDTO(HttpStatus.FORBIDDEN, "Invalid Credentials.");
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
+    }
+
+    // It captures errors due to annotations.
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            @NonNull MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request){
+        String messages = getValidationErrors(ex);
+        ErrorMessageDTO errorResponse = new ErrorMessageDTO(HttpStatus.BAD_REQUEST, messages);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    // Collects all error messages from annotations (Ex: @Size, @NotBlank)
+    private String getValidationErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage) // Usando a sugestão do IntelliJ
+                .collect(Collectors.joining(", "));
     }
 }
